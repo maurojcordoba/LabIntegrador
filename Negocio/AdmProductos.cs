f0817693711b8f1f8bf20c35c5ab7d8fe4cbcd3d
VSUPPORT <VSUPPORT@NT-DESA-05>
2016-10-25 11:37:47 -0300

    Agregar archivos de proyecto.

---
+++ Negocio/AdmProductos.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    class AdmProductos
+    {
+        public List<Producto> Listar()
+        {
+            return new List<Producto>();
+        }
+
+        //Lista de Productos
+        public List<Producto> Listar(string CUIT)
+        {
+            return new List<Producto>();
+        }
+    }
+}
