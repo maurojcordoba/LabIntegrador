a656642bb63a2a3943c1e15670e3373cb71519c5
maurojcordoba <mauro.j.cordoba@gmail.com>
2016-10-26 14:59:49 -0300

    Fase06 - Paso 5

---
+++ Presentacion/frmPrincipal.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Presentacion
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageClientes = new System.Windows.Forms.TabPage();
+            this.tabPageVentas = new System.Windows.Forms.TabPage();
+            this.tabPageProductos = new System.Windows.Forms.TabPage();
+            this.tabPageProveedores = new System.Windows.Forms.TabPage();
+            this.tabPageFacturas = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(394, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 326);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(394, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageVentas);
+            this.tabControl1.Controls.Add(this.tabPageProductos);
+            this.tabControl1.Controls.Add(this.tabPageClientes);
+            this.tabControl1.Controls.Add(this.tabPageProveedores);
+            this.tabControl1.Controls.Add(this.tabPageFacturas);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(394, 302);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPageClientes
+            // 
+            this.tabPageClientes.Location = new System.Drawing.Point(4, 22);
+            this.tabPageClientes.Name = "tabPageClientes";
+            this.tabPageClientes.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageClientes.Size = new System.Drawing.Size(386, 276);
+            this.tabPageClientes.TabIndex = 0;
+            this.tabPageClientes.Text = "Clientes";
+            this.tabPageClientes.UseVisualStyleBackColor = true;
+            // 
+            // tabPageVentas
+            // 
+            this.tabPageVentas.Location = new System.Drawing.Point(4, 22);
+            this.tabPageVentas.Name = "tabPageVentas";
+            this.tabPageVentas.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageVentas.Size = new System.Drawing.Size(386, 276);
+            this.tabPageVentas.TabIndex = 1;
+            this.tabPageVentas.Text = "Ventas";
+            this.tabPageVentas.UseVisualStyleBackColor = true;
+            // 
+            // tabPageProductos
+            // 
+            this.tabPageProductos.Location = new System.Drawing.Point(4, 22);
+            this.tabPageProductos.Name = "tabPageProductos";
+            this.tabPageProductos.Size = new System.Drawing.Size(386, 276);
+            this.tabPageProductos.TabIndex = 2;
+            this.tabPageProductos.Text = "Productos";
+            this.tabPageProductos.UseVisualStyleBackColor = true;
+            // 
+            // tabPageProveedores
+            // 
+            this.tabPageProveedores.Location = new System.Drawing.Point(4, 22);
+            this.tabPageProveedores.Name = "tabPageProveedores";
+            this.tabPageProveedores.Size = new System.Drawing.Size(386, 276);
+            this.tabPageProveedores.TabIndex = 3;
+            this.tabPageProveedores.Text = "Proveedores";
+            this.tabPageProveedores.UseVisualStyleBackColor = true;
+            // 
+            // tabPageFacturas
+            // 
+            this.tabPageFacturas.Location = new System.Drawing.Point(4, 22);
+            this.tabPageFacturas.Name = "tabPageFacturas";
+            this.tabPageFacturas.Size = new System.Drawing.Size(386, 276);
+            this.tabPageFacturas.TabIndex = 4;
+            this.tabPageFacturas.Text = "Facturas";
+            this.tabPageFacturas.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 348);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageClientes;
+        private System.Windows.Forms.TabPage tabPageVentas;
+        private System.Windows.Forms.TabPage tabPageProductos;
+        private System.Windows.Forms.TabPage tabPageProveedores;
+        private System.Windows.Forms.TabPage tabPageFacturas;
+    }
+}
+
---
+++ Presentacion/frmPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ProductName";
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPrincipal.ActiveForm.Close();
+        }
+    }
+}
